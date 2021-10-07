5fa82a25299e88337aed2715ebd6d1165f57aced
etiennebeaulieu <61425467+etiennebeaulieu@users.noreply.github.com>
2021-10-07 13:03:51 -0400

    Premier jet parcours

--- RobUS/src/main.cpp
+++ RobUS/src/main.cpp
@@ -4,6 +4,39 @@ void setup() {
   // put your setup code here, to run once:
   BoardInit();
   Serial.write(VERSIONID);
+
+  avancer(2400);
+  tourner(-90);
+  avancer(550);
+  tourner(90);
+  avancer(550);
+  tourner(90);
+  avancer(550);
+  tourner(-45);
+  avancer(750);
+  tourner(-90);
+  avancer(690);
+  tourner(30);
+  avancer(1120);
+
+
+
+  tourner(180);
+
+
+  avancer(1120);
+  tourner(-30);
+  avancer(690);
+  tourner(90);
+  avancer(750);
+  tourner(45);
+  avancer(550);
+  tourner(-90);
+  avancer(550);
+  tourner(-90);
+  avancer(550);
+  tourner(90);
+  avancer(2400;
 }
 
 void loop() {
