0140f6dea6b888efa207c9dfd27518d6c170b76c
cdg66 <claudecdg66@icloud.com>
2021-12-02 14:54:31 -0500

    Update drapeau.cpp

---
+++ RobUS/src/drapeau.cpp
@@ -0,0 +1,23 @@
+#include ""
+
+drapeaux_Init(void)
+{
+    SERVO_SetPWM(uint8_t ServoID, uint16_t pulselen)
+};
+drapeaux_Drop(uint8_t ServoID)
+{
+
+};
+drapeaux_Lock(uint8_t ServoID)
+{
+
+};
+
+drapeaux_DropAll()
+{
+
+};
+drapeaux_LockAll()
+{
+
+};
